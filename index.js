8c46265ac1d5455c0d839315c16b71e6aeecdcc0
demetrioscwb <demetrioscwb@gmail.com>
2018-11-18 17:52:43 -0200

    estudo 18/11/2018 17:52

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('test....');
+class Message{
+    constructor(text = '',created = Date.now()){
+        this.text = text;
+        this.created =created;
+    }
+    toString(){
+        return 'message to string....1';
+    }
+}
+
+class ImageMessage extends Message{
+    constructor(text = '', created = Date.now(), url = 'asdsad', thumbnail = ''){
+        super(text, created);
+        this.url = url;
+        this.thumbnail = thumbnail;
+    }
+    toString(){
+        return super.toString();
+    }
+}
+
+var emptyMessage = new Message();
+var textMessage = new Message('teste...', Date.now()-86400);
+var photoMessage = new ImageMessage();
+
+//console.log(emptyMessage.toString());
+//console.log(textMessage);
+console.log(photoMessage.toString());
